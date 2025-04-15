338ea46ef469d07f0e432354b0c502bf24818663
mafecabrales <maria.cabrales@mercadolibre.com.co>
2025-04-15 11:01:53 -0500

    Project base structure

---
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g04/BeJavaHispW31G04Application.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.be_java_hisp_w31_g04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaHispW31G04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeJavaHispW31G04Application.class, args);
+	}
+
+}
