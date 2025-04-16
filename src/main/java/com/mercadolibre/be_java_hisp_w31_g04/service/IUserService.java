d16a3bd0807e07a4c931372c84cfc90c31797181
dmojica1958 <dimojica@co000cxwg9m6yw4.ml.com>
2025-04-16 16:11:15 -0500

    feat: merge with develop

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g04/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.mercadolibre.be_java_hisp_w31_g04.service;
-
-import com.mercadolibre.be_java_hisp_w31_g04.dto.UserFollowedDto;
-
-public interface IUserService {
-    UserFollowedDto getUserFollowed(Integer userId, String order);
-}
