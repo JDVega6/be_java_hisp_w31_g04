bd7edd27a36dc75bccbe2d3fe0556185c488c747
jvegaguativa_meli <juan.vegaguativa@mercadolibre.com.co>
2025-04-20 17:49:22 -0500

    Add endpoint for creating product posts

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g04/model/Post.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g04/model/Post.java
@@ -1,15 +1,17 @@
 package com.mercadolibre.be_java_hisp_w31_g04.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class Post {
     private int userId;
     private int id;
     private LocalDate date;
     private Product product;
-    private String category;
+    private int category;
     private Double price;
 }
--- src/main/java/com/mercadolibre/be_java_hisp_w31_g04/repository/api/IProductRepository.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g04/repository/api/IProductRepository.java
@@ -1,4 +1,11 @@
 package com.mercadolibre.be_java_hisp_w31_g04.repository.api;
 
+import com.mercadolibre.be_java_hisp_w31_g04.model.Post;
+import com.mercadolibre.be_java_hisp_w31_g04.model.Product;
+
+import java.util.Optional;
+
 public interface IProductRepository {
+    Optional<Product> findProductById(int id);
+    void savePost(Post postProduct);
 }
