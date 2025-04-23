3a37902599ebd9a6c69d9464e05cc7625f6b8dad
mafecabrales <maria.cabrales@mercadolibre.com.co>
2025-04-22 21:03:26 -0500

    Removed imports not used

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g04/repository/api/IProductRepository.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g04/repository/api/IProductRepository.java
@@ -1,6 +1,5 @@
 package com.mercadolibre.be_java_hisp_w31_g04.repository.api;
 
-import com.mercadolibre.be_java_hisp_w31_g04.dto.ProductDto;
 import com.mercadolibre.be_java_hisp_w31_g04.model.Post;
 import com.mercadolibre.be_java_hisp_w31_g04.model.Product;
 
