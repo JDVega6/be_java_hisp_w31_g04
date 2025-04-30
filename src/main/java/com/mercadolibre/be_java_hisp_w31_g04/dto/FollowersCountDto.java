acc99b89e2161a56101be14f91fb8a610894bd77
mafecabrales <maria.cabrales@mercadolibre.com.co>
2025-04-30 11:00:17 -0500

    Fix: attributes naming in FollowersCountDto to lower camel case. JSON maintains snake case

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g04/dto/FollowersCountDto.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g04/dto/FollowersCountDto.java
@@ -1,12 +1,16 @@
 package com.mercadolibre.be_java_hisp_w31_g04.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class FollowersCountDto {
-    private int user_id;
-    private String user_name;
-    private int followers_count;
+    @JsonProperty("user_id")
+    private int userId;
+    @JsonProperty("user_name")
+    private String userName;
+    @JsonProperty("followers_count")
+    private int followersCount;
 }
